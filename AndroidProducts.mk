8887044c438f5e9eb5cb2c927e64d0360f774df2
f401 <nanLianQing@outlook.com>
2024-02-06 00:31:37 +0800

    First

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_cezanne.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_cezanne-user \
+    omni_cezanne-userdebug \
+    omni_cezanne-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Redmi M2006J10C (cezanne)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
